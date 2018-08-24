8748dfafa7a7cd64e41231f40eed1a80bc24fe4e
Chan Lee <chanethanlee@gmail.com>
2018-08-24 12:37:16 -0700

    edit text output function.:

--- js/scripts.js
+++ js/scripts.js
@@ -11,31 +11,46 @@ function Food(type, topping, price) {
   this.availalbe =true;
 }
 
+function replaceNull(...args) {
+    args.forEach(arg =>
+    !arg? arg = "None": arg);
+}
+
+function textConfirm(crust, topping, size){
+  replaceNull(crust, topping, size);
+  $(".crust").text(crust);
+  $(".topping").text(topping);
+  $(".size").text(size);
+  // $(".price").text(price);
+  $("#confirmation").show();
+}
+
 $(document).ready(function(){
   $("#pizza-builder").submit(function(event){
   event.preventDefault();
 
-  var crustInputText = crustArray[parseInt($("select#crust").is("selected").val())]
+  var crustInputText = crustArray[parseInt($("#crust").val())]
   console.log("crustInputText"+crustInputText);
 
   var toppingInputValue =[];
   var toppingInputText =[];
+
   $("input:checkbox[name=topping]:checked").each(function(){
-    toppingInputValue += parseInt(this.val());
-    toppingInputText += toppingArray[parseInt(this.val()];
+    toppingInputValue += parseInt($(this).val());
+    toppingInputText += toppingArray[parseInt($(this).val())] + " ";
+  });
 
   console.log("toppingInputValue"+toppingInputValue);
   console.log("toppingInputText"+toppingInputText);
 
-  var sizeInputText = sizeArray[parseInt($("input:radio[name=size]").is("checked").val())];
+  var sizeInputText = sizeArray[parseInt($("input:radio[name=size]:checked").val())];
   console.log("sizeInputText"+sizeInputText);
+
 
   // var priceInputText = this.price(...arg);
 
-  $(".crust").text(crustInputText);
-  $(".topping").text(toppingInputText);})
-  $(".size").text(sizeInputText);
-  $(".price").text(priceInputText);
+  textConfirm(crustInputText,toppingInputText,sizeInputText);
+  $("#pizza-builder")[0].reset();
 
   });
 
