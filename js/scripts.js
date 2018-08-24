4ee5f32ae117affa2015530ba6cf94a60d4bfe67
Chan Lee <chanethanlee@gmail.com>
2018-08-24 11:37:38 -0700

    edit user-interaction function for topping checkbox field.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,41 @@
-var available=true;
+var crustArray = ["Brooklyn Style","Hand Tossed","Crunchy Thin Crust"];
 
-var crustArray = ["Brooklyn Style","Hand Tossed","Crunchy Thin Crust"]
+var toppingArray = ["Pepperoni", "Beef", "Ham", "Philly Steak"];
 
-var toppingArray = ["Pepperoni", "Beef", "Ham", "Philly Steak"]
+var sizeArray = ["small", "medium", "large", "x-large"];
 
-function Food(type, topping, price, available) {
+function Food(type, topping, price) {
   this.type = type;
   this.topping = topping;
   this.price = price;
-  this.availalbe =available;
+  this.availalbe =true;
 }
 
 $(document).ready(function(){
   $("#pizza-builder").submit(function(event){
   event.preventDefault();
-  var crustInput = crustArray[parseInt($("select#crust").is("selected").val())]
-  console.log("crustInput"+crustInput);
-  var toppingInput = toppingArray[parseInt($("#topping-list input").is("checked").val())]
-  console.log("toppingInput"+toppingInput);
-  $(".crust").text(crustInput);
-  $(".topping").text(toppingInput);
-  $(".size").text(sizeInput);
-  $(".price").text(priceInput);
+
+  var crustInputText = crustArray[parseInt($("select#crust").is("selected").val())]
+  console.log("crustInputText"+crustInputText);
+
+  var toppingInputValue =[];
+  var toppingInputText =[];
+  $("input:checkbox[name=topping]:checked").each(function(){
+    toppingInputValue += parseInt(this.val());
+    toppingInputText += toppingArray[parseInt(this.val()];
+
+  console.log("toppingInputValue"+toppingInputValue);
+  console.log("toppingInputText"+toppingInputText);
+
+  var sizeInputText = sizeArray[parseInt($("input:radio[name=size]").is("checked").val())];
+  console.log("sizeInputText"+sizeInputText);
+
+  // var priceInputText = this.price(...arg);
+
+  $(".crust").text(crustInputText);
+  $(".topping").text(toppingInputText);})
+  $(".size").text(sizeInputText);
+  $(".price").text(priceInputText);
 
   });
 
