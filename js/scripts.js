b641d2ee4f22f5524b9624097ee2f743c3ce3a25
Chan Lee <chanethanlee@gmail.com>
2018-08-24 14:56:38 -0700

    seperate text output from calc function.:

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,41 @@
+//Arrays for pizza text outputs
 var crustArray = ["Brooklyn Style","Hand Tossed","Crunchy Thin Crust"];
-
 var toppingArray = ["Pepperoni", "Beef", "Ham", "Philly Steak"];
+var sizeArray = ["Small", "Medium", "Large", "X-Large"];
 
-var sizeArray = ["small", "medium", "large", "x-large"];
-
-function Food(type, topping, price) {
-  this.type = type;
-  this.topping = topping;
-  this.price = price;
-  this.availalbe =true;
+//Food class will be used to make menus including pizza.
+function Food(toppingList, toppingCount, size, basePrice) {
+  this.toppingList = toppingList;
+  this.toppingCount = toppingCount;
+  this.size = size;
+  this.price = basePrice;
+  this.available =true;
 }
 
-function replaceNull(...args) {
-    args.forEach(arg =>
-    !arg? arg = "None": arg);
+function Pizza(toppingList, toppingCount, size, basePrice, crust) {
+  Food.apply(this, arguments);
+  this.crust = crust;
 }
 
-function textConfirm(crust, topping, size){
-  replaceNull(crust, topping, size);
-  $(".crust").text(crust);
+//Create Pizza class from Food Class.
+// Pizza.prototype = Object.create(Food.prototype);
+// Pizza.prototype.constructor = Pizza;
+
+Pizza.prototype.calcPrice = function () {
+  this.price += (this.toppingCount-1)*2 + (this.size)*4 +(this.crust)*1;
+};
+
+
+function textConfirm(crust, topping, size, price){
+  var sizeInputText = sizeArray[size];
+  var crustInputText = crustArray[crust];
+  if (!topping) {
+    topping = "None";
+  }
+  $(".crust").text(crustInputText);
   $(".topping").text(topping);
-  $(".size").text(size);
-  // $(".price").text(price);
+  $(".size").text(sizeInputText);
+  $(".price").text(price);
   $("#confirmation").show();
 }
 
@@ -29,27 +43,37 @@ $(document).ready(function(){
   $("#pizza-builder").submit(function(event){
   event.preventDefault();
 
-  var crustInputText = crustArray[parseInt($("#crust").val())]
+  var crustInputValue = parseInt($("#crust").val());
+  var crustInputText = crustArray[crustInputValue]
   console.log("crustInputText"+crustInputText);
 
-  var toppingInputValue =[];
+  var toppingCount=0;
   var toppingInputText =[];
 
   $("input:checkbox[name=topping]:checked").each(function(){
-    toppingInputValue += parseInt($(this).val());
-    toppingInputText += toppingArray[parseInt($(this).val())] + " ";
+    toppingCount++;
+    toppingInputText.push(" "+toppingArray[parseInt($(this).val())]);
   });
+  typeof topppingInputText;
+  typeof topppingCount;
 
-  console.log("toppingInputValue"+toppingInputValue);
+  console.log("toppingCount"+toppingCount);
   console.log("toppingInputText"+toppingInputText);
 
-  var sizeInputText = sizeArray[parseInt($("input:radio[name=size]:checked").val())];
-  console.log("sizeInputText"+sizeInputText);
+  var sizeInputValue = parseInt($("input:radio[name=size]:checked").val());
+
+  var pizzaInstance = new Pizza (toppingInputText, toppingCount, sizeInputValue, 8, crustInputValue);
+  console.log(pizzaInstance.crust)
+  console.log(pizzaInstance.toppingList);
+  console.log(pizzaInstance.toppingCount)
+  console.log(pizzaInstance.price)
+
+  pizzaInstance.calcPrice();
+  textConfirm(pizzaInstance.crust, pizzaInstance.toppingList, pizzaInstance.size, pizzaInstance.price);
 
 
   // var priceInputText = this.price(...arg);
 
-  textConfirm(crustInputText,toppingInputText,sizeInputText);
   $("#pizza-builder")[0].reset();
 
   });
