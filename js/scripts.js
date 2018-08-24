248419d98c2fa330cd5463d3992ca1d57134043b
Chan Lee <chanethanlee@gmail.com>
2018-08-24 10:50:32 -0700

    add submit function with crust/size option and relevant value arrays.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,35 @@
 var available=true;
 
+var crustArray = ["Brooklyn Style","Hand Tossed","Crunchy Thin Crust"]
+
+var toppingArray = ["Pepperoni", "Beef", "Ham", "Philly Steak"]
+
 function Food(type, topping, price, available) {
   this.type = type;
   this.topping = topping;
   this.price = price;
   this.availalbe =available;
 }
+
+$(document).ready(function(){
+  $("#pizza-builder").submit(function(event){
+  event.preventDefault();
+  var crustInput = crustArray[parseInt($("select#crust").is("selected").val())]
+  console.log("crustInput"+crustInput);
+  var toppingInput = toppingArray[parseInt($("#topping-list input").is("checked").val())]
+  console.log("toppingInput"+toppingInput);
+  $(".crust").text(crustInput);
+  $(".topping").text(toppingInput);
+  $(".size").text(sizeInput);
+  $(".price").text(priceInput);
+
+  });
+
+
+
+// For addtional form
+// $("#").submit(function(event){
+//   event.preventDefault();
+// });
+
+});
