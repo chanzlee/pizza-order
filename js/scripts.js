71d5e43f4b31e015752edfe15c6eee02a6044618
Chan Lee <chanethanlee@gmail.com>
2018-08-24 21:57:38 -0700

    add pizza adding functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ var crustArray = ["Brooklyn Style","Hand Tossed","Crunchy Thin Crust"];
 var toppingArray = ["Pepperoni", "Beef", "Ham", "Philly Steak"];
 var sizeArray = ["Small", "Medium", "Large", "X-Large"];
 
+
+var orders= {};
+var orderIndex = 0;
+
 //Food class will be used to make menus including pizza.
 function Food(toppingList, toppingCount, size, basePrice) {
   this.toppingList = toppingList;
@@ -25,24 +29,35 @@ Pizza.prototype.calcPrice = function () {
   this.price += (this.toppingCount-1)*2 + (this.size)*4 +(this.crust)*1;
 };
 
-
-function textConfirm(crust, topping, size, price){
+function textConfirm(crust, topping, size, price, addConfirm){
   var sizeInputText = sizeArray[size];
   var crustInputText = crustArray[crust];
   if (!topping) {
     topping = "None";
   }
-  $(".crust").text(crustInputText);
-  $(".topping").text(topping);
-  $(".size").text(sizeInputText);
-  $(".price").text(price);
-  $("#confirmation").show();
+  orderIndex++;
+  console.log(orderIndex);
+  confirmAppender();
+
+    $("div.append:last").html(addConfirm);
+  $(".append:last-child .index").text(orderIndex);
+  $(".append:last-child .crust").text(crustInputText);
+  $(".append:last-child .topping").text(topping);
+  $(".append:last-child .size").text(sizeInputText);
+  $(".append:last-child .price").text(price);
+}
+
+function confirmAppender(){
+  $("#confirm-list").append("<div class='append'></div>");
 }
 
 $(document).ready(function(){
+  $(".confirm").hide();
+  var addConfirm = $(".confirm").html();
+
   $("#pizza-builder").submit(function(event){
   event.preventDefault();
-
+  console.log("submit working");
   var crustInputValue = parseInt($("#crust").val());
   var crustInputText = crustArray[crustInputValue]
 
@@ -61,7 +76,8 @@ $(document).ready(function(){
   var pizzaInstance = new Pizza (toppingInputText, toppingCount, sizeInputValue, 8, crustInputValue);
 
   pizzaInstance.calcPrice();
-  textConfirm(pizzaInstance.crust, pizzaInstance.toppingList, pizzaInstance.size, pizzaInstance.price);
+
+  textConfirm(pizzaInstance.crust, pizzaInstance.toppingList, pizzaInstance.size, pizzaInstance.price, addConfirm);
 
 
   // var priceInputText = this.price(...arg);
@@ -70,6 +86,9 @@ $(document).ready(function(){
 
   });
 
+  $("#add").click(function(){
+
+  });
 
 
 // For addtional form
